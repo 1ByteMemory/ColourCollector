bea4ca86b610c2942bef0bea993fa56c58a7eff5
Dan J <dan.jones8902@gmail.com>
2020-11-03 14:49:30 +0000

    Made Stats variables accessable through string names

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -1,19 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerWeapon : MonoBehaviour
 {
 
-    public Stats BaseStats;
+    public Stats stats;
 
+    string _name = "Damage";
     //public Stats[] ColorModifiers;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Stats.SetVariable(stats, _name, 10);
+        Debug.Log(Stats.GetValue(stats, _name).ToString());
     }
 
     // Update is called once per frame
--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -1,14 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
 
 [Serializable]
 public class Stats
 {
-    public int damage;
-    public int shotSpeed;
-    public int fireingSpeed;
-    public int shots;
-    public int health;
-    public int moveSpeed;
+	private int damage;
+    private int shotSpeed;
+    private int firingSpeed;
+    private int shots;
+    private int health;
+    private int moveSpeed;
+	public int Damage
+	{
+		get { return damage; }
+		set { damage = value; }
+	}
+	public int ShotSpeed
+	{
+		get { return shotSpeed; }
+		set { shotSpeed = value; }
+	}
+	public int FiringSpeed
+	{
+		get { return firingSpeed; }
+		set { firingSpeed = value; }
+	}
+	public int Shots
+	{
+		get { return shots; }
+		set { shots = value; }
+	}
+	public int Health
+	{
+		get { return health; }
+		set { health = value; }
+	}
+	public int MoveSpeed
+	{
+		get { return moveSpeed; }
+		set { moveSpeed = value; }
+	}
+
+
+	public static object GetValue(object stats, string variable)
+	{
+		Type type = stats.GetType();
+		PropertyInfo info = type.GetProperty(variable, BindingFlags.Public | BindingFlags.Instance);
+		
+		return info.GetValue(stats);
+	}
+
+	public static void SetVariable(object stats, string variable, int value)
+	{
+		Type type = stats.GetType();
+		PropertyInfo info = type.GetProperty(variable, BindingFlags.Public | BindingFlags.Instance);
+
+		info.SetValue(stats, value);
+
+	}
+
+}
+
+
+public class Module
+{
+	public string name;
+	public Dictionary<string, int> Modifiers;
+
+	
+
 
 }
