a2c2c6307646a1d771a666fea59e99b35bc8d5ee
Dan J <dan.jones8902@gmail.com>
2020-11-17 16:31:58 +0000

    Adding damage display numbers

---
+++ Assets/Scripts/DamageNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageNumbers : MonoBehaviour
+{
+    public float duration;
+    public string dispalyText;
+    public Color color;
+
+	Text text;
+
+	float startTime;
+	
+	private void OnEnable()
+	{
+		text = GetComponent<Text>();
+		text.color = color;
+		text.text = dispalyText;
+
+		startTime = Time.time;
+	}
+
+
+	// Update is called once per frame
+	void Update()
+    {
+		transform.Translate(Vector3.up * Time.deltaTime);
+
+		float alpha = (Time.time - startTime) / duration;
+		Color col = text.color;
+
+		col.a = alpha;
+
+		text.color = col;
+
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+	[HideInInspector]
+	public float speed, range;
+
+	Vector3 startPos;
+	Vector3 currPos;
+	private void OnEnable()
+	{
+		startPos = transform.position;
+	}
+
+	private void Update()
+	{
+		transform.Translate(Vector3.up * speed * Time.deltaTime);
+		currPos = transform.position;
+
+		if (Vector3.Distance(startPos, currPos) >= range)
+		{
+			Destroy(gameObject);
+		}
+
+	}
+}
